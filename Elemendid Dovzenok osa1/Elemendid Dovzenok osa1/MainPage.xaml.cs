2d69cbf0abe69e8a47d9be46f04135478a1c6f76
opilane <opilane@HP-CZC6297BBB>
2020-11-11 11:29:15 +0200

    add image

--- Elemendid Dovzenok osa1/Elemendid Dovzenok osa1/MainPage.xaml.cs
+++ Elemendid Dovzenok osa1/Elemendid Dovzenok osa1/MainPage.xaml.cs
@@ -10,17 +10,39 @@ namespace Elemendid_Dovzenok_osa1
 {
     public partial class MainPage : ContentPage
     {
+        private object editor;
+        private int picker_SelectedIndex;
+
         public MainPage()
         {
-            InitializeComponent();
+     
+        }
+        Image img = new Image();
+        private void picker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (picker_SelectedIndex == 0)
+            {
+                img.Source = "valga.png";
+            }
+            if (picker_SelectedIndex == 1)
+            {
+                img.Source = "viljandi.png";
+            }
+            if (picker_SelectedIndex == 1)
+            {
+                img.Source = "võru.png";
+            }
+            if (picker_SelectedIndex == 1)
+            {
+                img.Source = "ida.png";
+            }
+            if (picker_SelectedIndex == 1)
+            {
+                img.Source = "jõgeva.png";
+            }
         }
 
         private void picker1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void picker_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
---
+++ Elemendid Dovzenok osa1/Elemendid Dovzenok osa1/img.cs
@@ -0,0 +1,7 @@
+﻿namespace Elemendid_Dovzenok_osa1
+{
+    internal class img
+    {
+        internal static string Source;
+    }
+}
