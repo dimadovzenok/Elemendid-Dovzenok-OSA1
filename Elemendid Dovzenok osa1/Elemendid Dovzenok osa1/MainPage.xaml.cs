361522d4d52ff7380e23d8ce122a9b71f5c1534b
opilane <opilane@HP-CZC6297BBB>
2020-11-11 10:53:45 +0200

    Add project files.

---
+++ Elemendid Dovzenok osa1/Elemendid Dovzenok osa1/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Elemendid_Dovzenok_osa1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void picker1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
